41beeddc2fcbb72e7e30edf8ad6af0ab60fe406f
Łukasz Świszcz <luk.swiszcz@gmail.com>
2024-11-25 23:16:27 +0100

    Fix code formatting

--- Firmware/Src/rtos.cpp
+++ Firmware/Src/rtos.cpp
@@ -10,14 +10,14 @@
 extern "C" void rtos_main()
 {
 #ifdef _DEBUG
-// NOLINTBEGIN(*)
+    // NOLINTBEGIN(*)
 
     // https://community.st.com/t5/stm32-mcus-products/
     // stm32f7-debugging-stepping-impossible-ide-jumps-into-interrupts/m-p/447639/highlight/true#M143597
 
     DBGMCU->APB1FZ = 0x7E01BFF;
     DBGMCU->APB2FZ = 0x70003;
-    
+
 // NOLINTEND(*)
 #endif
 
