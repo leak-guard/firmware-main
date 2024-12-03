d801a3d3d4225f32bfaa972bcb372ddcfd4b7cf4
Monczak <szymon.jedrzejczak.sj@gmail.com>
2024-12-03 16:42:25 +0100

    Implement basic leak-logic-mgr functionality

--- Firmware/Inc/leak-logic-mgr.hpp
+++ Firmware/Inc/leak-logic-mgr.hpp
@@ -11,10 +11,21 @@ public:
     void initialize();
 
 private:
-    LeakLogic leakLogic;
-    SensorState sensorState;
+    static constexpr auto UPDATE_INTERVAL_MS = 60000;
 
-    void update();
+    LeakLogic leakLogic;
+    SensorState sensorState { .flowRate=0, .probeStates=StaticVector<bool, 256>() };
+
+    time_t lastUpdateTime = 0;
+
+    void leakLogicManagerMain();
+    void updateSensorState();
+    void updateLeakLogic();
+
+    bool addCriterion(std::unique_ptr<LeakDetectionCriterion> criterion) { return leakLogic.addCriterion(std::move(criterion)); }
+    StaticVector<std::unique_ptr<LeakDetectionCriterion>, LEAK_LOGIC_MAX_CRITERIA>::Iterator getCriteria() { return leakLogic.getCriteria(); }
+    bool removeCriterion(const uint8_t index) { return leakLogic.removeCriterion(index); }
+    void clearCriteria() { leakLogic.clearCriteria(); }
 };
 
 }
--- Firmware/Src/leak-logic-mgr.cpp
+++ Firmware/Src/leak-logic-mgr.cpp
@@ -1,1 +1,47 @@
+#include <device.hpp>
 #include <leak-logic-mgr.hpp>
+
+namespace lg {
+
+void LeakLogicManager::leakLogicManagerEntryPoint(void* params)
+{
+    auto instance = reinterpret_cast<LeakLogicManager*>(params);
+    instance->leakLogicManagerMain();
+}
+
+void LeakLogicManager::initialize()
+{
+    lastUpdateTime = Device::get().getLocalTime().toTimestamp();
+}
+
+void LeakLogicManager::leakLogicManagerMain()
+{
+    while (true) {
+        updateSensorState();
+        updateLeakLogic();
+
+        vTaskDelay(UPDATE_INTERVAL_MS);
+    }
+}
+
+void LeakLogicManager::updateSensorState()
+{
+    auto flowMeterService = Device::get().getFlowMeterService();
+    sensorState.flowRate = flowMeterService->getCurrentFlowInMlPerMinute() / 1000.0f;
+
+    // TODO: Hook up probe service
+}
+
+void LeakLogicManager::updateLeakLogic()
+{
+    auto currentTime = Device::get().getLocalTime().toTimestamp();
+    leakLogic.update(sensorState, currentTime - lastUpdateTime);
+    lastUpdateTime = currentTime;
+
+    auto action = leakLogic.getAction();
+    if (action.getActionType() != ActionType::NO_ACTION) {
+        // TODO: Alert
+    }
+}
+
+}
